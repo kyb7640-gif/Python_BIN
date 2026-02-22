051f1669cb149873429eb07be034cdcd511774a3
kyb7640-gif <kyb7640@gmail.com>
2026-02-23 08:48:59 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    sum = 0
+    if n%2!=0:
+        for i in range(1,n+1):
+            if(i%2!=0):
+                sum+=i
+        return sum
+    else:
+        for i in range(1,n+1):
+            if(i%2==0):
+                sum+=(i*i)
+        return sum
+                
