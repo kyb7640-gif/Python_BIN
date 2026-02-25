10ddfd8cd5a1acd1ae7aa787b418cf6280fa9e9a
kyb7640-gif <kyb7640@gmail.com>
2026-02-25 14:01:55 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.c
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.c
@@ -1,46 +1,100 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#define MAX 200
 
-int main(void){
-    int cnt,i;
-    int isright;
-    char str[102];
-    char stack[102];
-    
-    while(!0){
-        cnt = -1;
-        isright = 1;
-        fgets(str,102,stdin);
-        if(str[0]=='.') break;
-        str[strlen(str)-1]='\0';
-        
-        for(i=0;i<strlen(str);i++){
-            if(str[i]=='(' || str[i]=='['){
-                stack[++cnt] = str[i];
+typedef struct {
+   char data[MAX];
+   int top;
+   int size;
+}Stack;
+
+int init(Stack* s) {
+   s->top = -1;
+   return 0;
+}
+
+int full(Stack* s) {
+   if (s->top == MAX - 1) {
+      return 1;
+   }
+   return 0;
+}
+
+int empty(Stack* s) {
+   if (s->top == -1) {
+      return 1;
+   }
+   return 0;
+}
+
+char push(Stack* s, char value) {
+   if (full(s) == 1) return -1;
+   s->size++;
+   return s->data[++(s->top)] = value;
+   
+}
+
+char pop(Stack* s) {
+   if (empty(s) == 1) return -1;
+   s->size--;
+   return s->data[(s->top)--];
+}
+
+char peek(Stack* s) {
+   if (empty(s) == 1) return -1;
+   return s->data[s->top];
+}
+
+
+int main() {
+   Stack s;
+   init(&s);
+   char str[102];
+   int i = 0, swi = 0;
+
+   while (1) {
+      fgets(str, 102, stdin);
+      str[strcspn(str, "\n")] = '\0';
+      if (strcmp(str, ".") == 0) break;
+      i = 0;
+      init(&s);
+      swi = 0;
+   
+      while (str[i] != '\0') {
+         if (str[i] == '(') {
+            push(&s, str[i]);
+         }
+         if (str[i] == '[') {
+            push(&s, str[i]);
+         }
+         if (str[i] == ')') {
+            if (empty(&s) == 1 || peek(&s) != '(') {
+               swi = 1;
+               break;
             }
-            else if(str[i]==')'){
-                if(cnt>=0&&stack[cnt]=='('){
-                    cnt--;
-                }
-                else{
-                    isright = 0;
-                    break;
-                }
+            pop(&s);
+            
+            
+         }
+         if (str[i] == ']') {
+            if (empty(&s) == 1 || peek(&s) != '[') {
+               swi = 1;
+               break;
             }
-            else if(str[i]==']'){
-                if(cnt>=0&&stack[cnt]=='['){
-                    cnt--;
-                }
-                else{
-                    isright = 0;
-                    break;
-                }
-            }
-        }
-        
-        if(isright==1 && cnt==-1) printf("yes\n");
-        else printf("no\n");
-    }
-    
-    return 0;
+
+            
+            pop(&s);
+            
+         }
+
+         i++;
+      }
+      //if (str[0] == '.') return 0;
+      if (empty(&s) == 1 && swi == 0) printf("yes\n");
+      else printf("no\n");
+     
+   }
+   return 0;
 }
